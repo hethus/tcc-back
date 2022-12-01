10f466e5d9990162e7e4a429d5e776e0d2b4790a
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-30 23:35:14 -0300

    chore: add new import from module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { PublicModule } from './public/public.module';
 import { AuthModule } from './auth/auth.module';
 import { ClasseModule } from './classe/classe.module';
+import { ClassesRelationModule } from './classes-relation/classes-relation.module';
 
 @Module({
-  imports: [UserModule, PublicModule, AuthModule, ClasseModule],
+  imports: [UserModule, PublicModule, AuthModule, ClasseModule, ClassesRelationModule],
   controllers: [AppController],
   providers: [AppService],
 })
