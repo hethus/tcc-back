3c3f8e3d6f8d2ec1889bc04207ef8f13c0e86fb7
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-30 23:36:23 -0300

    chore: implement classe module

--- src/classe/classe.module.ts
+++ src/classe/classe.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ClasseService } from './classe.service';
 import { ClasseController } from './classe.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [ClasseController],
-  providers: [ClasseService],
+  providers: [ClasseService, JwtStrategy],
 })
 export class ClasseModule {}
