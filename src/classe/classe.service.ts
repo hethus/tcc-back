f3e9653788cd515e4ea807f6ee5a9894112efd96
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-30 23:36:46 -0300

    feat: create classe service crud

--- src/classe/classe.service.ts
+++ src/classe/classe.service.ts
@@ -1,26 +1,82 @@
 import { Injectable } from '@nestjs/common';
 import { CreateClasseDto } from './dto/create-classe.dto';
 import { UpdateClasseDto } from './dto/update-classe.dto';
+import { User } from 'src/user/entities/user.entity';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/errorHandlers/customErrorList';
 
 @Injectable()
 export class ClasseService {
-  create(createClasseDto: CreateClasseDto) {
-    return 'This action adds a new classe';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(dto: CreateClasseDto, userLogged: User) {
+    const data = {
+      name: dto.name,
+      semester: dto.semester,
+      subjectId: dto.subjectId,
+      subjectName: dto.subjectName,
+      teacherId: userLogged.id,
+    };
+
+    return this.prisma.subjectClass
+      .create({
+        data,
+      })
+      .then((classe) => {
+        return classe;
+      })
+      .catch(handleError);
   }
 
-  findAll() {
-    return `This action returns all classe`;
+  findAll(email: string) {
+    return this.prisma.subjectClass
+      .findMany({
+        where: {
+          teacher: {
+            email,
+          },
+        },
+      })
+      .then((classes) => classes)
+      .catch(handleError);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} classe`;
+  findOne(id: string) {
+    return this.prisma.subjectClass
+      .findUnique({
+        where: {
+          id,
+        },
+      })
+      .then((classe) => classe)
+      .catch(handleError);
   }
 
-  update(id: number, updateClasseDto: UpdateClasseDto) {
-    return `This action updates a #${id} classe`;
+  update(id: string, dto: UpdateClasseDto) {
+    return this.prisma.subjectClass
+      .update({
+        where: {
+          id,
+        },
+        data: {
+          name: dto.name,
+          semester: dto.semester,
+          subjectId: dto.subjectId,
+          subjectName: dto.subjectName,
+        },
+      })
+      .then((classe) => classe)
+      .catch(handleError);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} classe`;
+  delete(id: string) {
+    return this.prisma.subjectClass
+      .delete({
+        where: {
+          id,
+        },
+      })
+      .then((classe) => classe)
+      .catch(handleError);
   }
 }
