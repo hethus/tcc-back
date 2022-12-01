0661f8def033e12a36b039d66eeffc4334f890b3
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-30 23:36:01 -0300

    feat: create classe controller crud

--- src/classe/classe.controller.ts
+++ src/classe/classe.controller.ts
@@ -6,11 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/user/entities/user.entity';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
+import { isAllowed } from 'src/lib/authLib';
+import enums from '../lib/enumLib';
 import { ClasseService } from './classe.service';
 import { CreateClasseDto } from './dto/create-classe.dto';
 import { UpdateClasseDto } from './dto/update-classe.dto';
+
+const { userType } = enums;
 
 @ApiTags('Classes')
 @Controller('classe')
@@ -18,27 +26,43 @@ export class ClasseController {
   constructor(private readonly classeService: ClasseService) {}
 
   @Post()
-  create(@Body() createClasseDto: CreateClasseDto) {
-    return this.classeService.create(createClasseDto);
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new classe' })
+  create(@Body() dto: CreateClasseDto, @LoggedUser() userLogged: User) {
+    isAllowed([userType.admin.value, userType.teacher.value], userLogged);
+    return this.classeService.create(dto, userLogged);
   }
 
-  @Get()
-  findAll() {
-    return this.classeService.findAll();
+  @Get(':email')
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Find all classes by user email' })
+  findAll(@Param('email') email: string) {
+    return this.classeService.findAll(email);
   }
 
-  @Get(':id')
+  @Get('one/:id')
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Find classe by id' })
   findOne(@Param('id') id: string) {
-    return this.classeService.findOne(+id);
+    return this.classeService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateClasseDto: UpdateClasseDto) {
-    return this.classeService.update(+id, updateClasseDto);
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Updated class by id' })
+  update(@Param('id') id: string, @Body() dto: UpdateClasseDto) {
+    return this.classeService.update(id, dto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.classeService.remove(+id);
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Delete class by id' })
+  delete(@Param('id') id: string) {
+    return this.classeService.delete(id);
   }
 }
