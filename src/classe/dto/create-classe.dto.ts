439596ed0555983634d844440091b8727ecbd6e4
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-11-30 23:37:04 -0300

    chore: create classe dto

--- src/classe/dto/create-classe.dto.ts
+++ src/classe/dto/create-classe.dto.ts
@@ -1,1 +1,36 @@
-export class CreateClasseDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateClasseDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Classe name',
+    example: 'Turma 1',
+  })
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Discipline name',
+    example: 'Math',
+  })
+  subjectName: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Discipline code',
+    example: '543642',
+  })
+  subjectId: number;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: 'Semester data',
+    example: '06-2021',
+  })
+  semester: string;
+}
