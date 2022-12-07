99cd7ededfeed5127b6c5cee3842b9d10b3a00f9
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-12-06 23:08:44 -0300

    feat: create Post

--- src/classes-relation/classes-relation.controller.ts
+++ src/classes-relation/classes-relation.controller.ts
@@ -1,7 +1,24 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { isAllowed } from 'src/lib/authLib';
 import { ClassesRelationService } from './classes-relation.service';
+import { User } from 'src/user/entities/user.entity';
+import enums from '../lib/enumLib';
 import { CreateClassesRelationDto } from './dto/create-classes-relation.dto';
+import { LoggedUser } from 'src/auth/logged-user.decorator';
 
+const { userType } = enums;
+
+@ApiTags('Classes-Relation')
 @Controller('classes-relation')
 export class ClassesRelationController {
   constructor(
@@ -9,8 +26,15 @@ export class ClassesRelationController {
   ) {}
 
   @Post()
-  create(@Body() createClassesRelationDto: CreateClassesRelationDto) {
-    return this.classesRelationService.create(createClassesRelationDto);
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Create a new relation' })
+  create(
+    @Body() dto: CreateClassesRelationDto,
+    @LoggedUser() userLogged: User,
+  ) {
+    isAllowed([userType.admin.value, userType.teacher.value], userLogged);
+    return this.classesRelationService.create(dto, userLogged);
   }
 
   @Get()
