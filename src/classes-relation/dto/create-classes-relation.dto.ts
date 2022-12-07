86e1a051cb6dacc3b2ad1f4c5bbbb1690d3e6fbd
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-12-06 23:09:22 -0300

    chore: create dto

--- src/classes-relation/dto/create-classes-relation.dto.ts
+++ src/classes-relation/dto/create-classes-relation.dto.ts
@@ -1,1 +1,12 @@
-export class CreateClassesRelationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateClassesRelationDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({
+    description: '',
+    example: '',
+  })
+  assignedBy: string;
+}
