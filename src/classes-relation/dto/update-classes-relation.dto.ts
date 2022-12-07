43aa44800f273c1b619375fbe539a550b92b9245
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-12-06 23:09:29 -0300

    delete file

--- src/classes-relation/dto/update-classes-relation.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateClassesRelationDto } from './create-classes-relation.dto';
-
-export class UpdateClassesRelationDto extends PartialType(CreateClassesRelationDto) {}
