4d4644dfc58cddcb8db407475359f2e227190364
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-12-11 23:19:12 -0300

    feat: create delete and findOne service

--- src/classes-relation/classes-relation.service.ts
+++ src/classes-relation/classes-relation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateClassesRelationDto } from './dto/create-classes-relation.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { User } from 'src/user/entities/user.entity';
@@ -9,27 +9,74 @@ export class ClassesRelationService {
   constructor(private readonly prisma: PrismaService) {}
 
   create(dto: CreateClassesRelationDto, userLogged: User) {
+    const userVerify = this.prisma.user.findUnique({
+      where: {
+        id: dto.userId,
+      },
+    });
+
+    if (!userVerify) {
+      throw new BadRequestException('Usuário não encontrado!');
+    }
+
+    const classVerify = this.prisma.subjectClass.findUnique({
+      where: {
+        id: dto.subjectClassId,
+      },
+    });
+
+    if (!classVerify) {
+      throw new BadRequestException('Classe não encontrada!');
+    }
+
     return this.prisma.usersSubjectClasses
       .create({
         data: {
-          assignedBy: dto.assignedBy,
-          userId: userLogged.name,
-          subjectClassId: userLogged.id,
+          assignedBy: userLogged.name,
+          user: {
+            connect: {
+              id: dto.userId,
+            },
+          },
+          subjectClass: {
+            connect: {
+              id: dto.subjectClassId,
+            },
+          },
+        },
+      })
+      .then((relation) => {
+        console.log(relation);
+        return relation;
+      })
+      .catch(handleError);
+  }
+
+  findOne(id: string) {
+    return this.prisma.usersSubjectClasses
+      .findUnique({
+        where: {
+          userId_subjectClassId: {
+            userId: id,
+            subjectClassId: id,
+          },
         },
       })
       .then((relation) => relation)
       .catch(handleError);
   }
 
-  findAll() {
-    return `This action returns all classesRelation`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} classesRelation`;
-  }
-
-  delete(id: number) {
-    return `This action removes a #${id} classesRelation`;
+  delete(id: string) {
+    return this.prisma.usersSubjectClasses
+      .delete({
+        where: {
+          userId_subjectClassId: {
+            userId: id,
+            subjectClassId: id,
+          },
+        },
+      })
+      .then((relation) => relation)
+      .catch(handleError);
   }
 }
